eddb9a33fcb5165fc1dd30eef682fdf54b8b3d54
karanu-171 <karanujohn171@gmail.com>
2023-07-14 12:01:05 +0300

    f commit

--- frontend/src/components/redux/store.jsx
+++ frontend/src/components/redux/store.jsx
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import contactSlice from "../features/contact/contactSlice";
 import userSlice from "../features/user/userSlice";
 import parcelSlice from "../features/parcel/parcelSlice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
-    contact: contactSlice,
     parcel: parcelSlice
   },
 });
